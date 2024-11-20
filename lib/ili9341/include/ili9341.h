fe886b5dda1b0c82f8e152366c2a0e626337ad1a
plebish <jasoncast880@gmail.com>
2024-11-20 00:04:01 -0500

    update the ili9341

--- lib/ili9341/include/ili9341.h
+++ lib/ili9341/include/ili9341.h
@@ -2,7 +2,7 @@
 #include "hardware/spi.h"
 
 /*
- * st7735 shouuld control all of the reg hardware assignments and 
+ * ili9341 shouuld control all of the reg hardware assignments and 
  * be an abstraction layer for all low level writes and commands..
  * although i will be writing app code in c
  * the driver should have a setup that can later be integrated into cpp
@@ -12,36 +12,35 @@
 
 //gp defines; 
 //
-//st7735 modes, reg addressing refer to strionix st7735 10.1 table
+//ili9341 modes, reg addressing refer to strionix ili9341 10.1 table
 
-//gp regs
-#define NOP 0x00
+//general-purpose registers
+#define NOP     0x00
 #define SWRESET 0x01
-#define RDDID 0x04
-#define RDDST 0x09
-
-//modes
+#define RDDID   0x04
+#define RDDST   0x09
 
 //???
-#define RDDPM 0x0A
+#define RDDPM   0x0A
 #define RDD_MADCTL 0x0B
 #define RDD_COLMOD 0x0C
-#define RDDIM 0x0D
-#define RDDSM 0x0E
+#define RDDIM   0x0D
+#define RDDSM   0x0E
 
 //modes
-#define SLPIN 0x10
-#define SLPOUT 0x11
-#define PTLON 0x12
-#define NORON 0x13
-#define INVOFF 0x20
-#define INVON 0x21
-#define GAMSET 0x26
+#define SLPIN   0x10
+#define SLPOUT  0x11
+#define PTLON   0x12
+#define NORON   0x13
+#define INVOFF  0x20
+#define INVON   0x21
+#define GAMSET  0x26
 #define DISPOFF 0x28
-#define DISPON 0x29
+#define DISPON  0x29
 
-#define CASET 0x2A
-#define RASET 0x2B
+//row, column set registers
+#define CASET   0x2A
+#define RASET   0x2B
 //there are more...
 
 //frame rate control
@@ -49,28 +48,39 @@
 //color control
 //mem access data control registers
 
-//gpio abstraction function implementations
-#define _DC_INIT gpio_init(_ST7735_DC)
-#define _RST_INIT gpio_init(_ST7735_RST)
-#define _CS_INIT gpio_init(_ST7735_CS)
-#define _SCLK_INIT gpio_init(_ST7735_SCLK)
-#define _SDATA_INIT gpio_init(_ST7735_SDATA)
+//define subroutine initialization sequnces here
+#define PWR_INIT_SEQ_1 {1,2,3,4} //example
+#define PWR_INIT_SEQ_2 {1,2,3,4} //example
 
-#define _DC_SetDigitalOut gpio_set_dir(_ST7735_DC, GPIO_OUT)
-#define _RST_SetDigitalOut gpio_set_dir(_ST7735_RST, GPIO_OUT)
-#define _CS_SetDigitalOut gpio_set_dir(_ST7735_CS, GPIO_OUT)
-#define _SCLK_SetDigitalOut gpio_set_dir(_ST7735_SCLK, GPIO_OUT)
-#define _SDATA_SetDigitalOut gpio_set_dir(_ST7735_SDATA, GPIO_OUT)
+#define VRAM_INIT_SEQ_1 {1,2,3,4} //example
+#define VRAM_INIT_SEQ_2 {1,2,3,4} //example
+                                  
+#define GAMMA_CORR_INIT_SEQ_1 {1,2,3,4} //example
+
+//gpio abstraction function implementations
+#define _DC_INIT gpio_init(_ILI9341_DC)
+#define _RST_INIT gpio_init(_ILI9341_RST)
+#define _CS_INIT gpio_init(_ILI9341_CS)
+#define _SCLK_INIT gpio_init(_ILI9341_SCLK)
+#define _SDATA_INIT gpio_init(_ILI9341_SDATA)
+
+#define _DC_SetDigitalOut gpio_set_dir(_ILI9341_DC, GPIO_OUT)
+#define _RST_SetDigitalOut gpio_set_dir(_ILI9341_RST, GPIO_OUT)
+#define _CS_SetDigitalOut gpio_set_dir(_ILI9341_CS, GPIO_OUT)
+#define _SCLK_SetDigitalOut gpio_set_dir(_ILI9341_SCLK, GPIO_OUT)
+#define _SDATA_SetDigitalOut gpio_set_dir(_ILI9341_SDATA, GPIO_OUT)
 
 //MISO not used. keep in mind
 
-#define _SCLK_SPI_FUNC gpio_set_function(_ST7735_SCLK, GPIO_FUNC_SPI)
-#define _SDATA_SPI_FUNC gpio_set_function(_ST7735_SDATA, GPIO_FUNC_SPI)
+#define _SCLK_SPI_FUNC gpio_set_function(_ILI9341_SCLK, GPIO_FUNC_SPI)
+#define _SDATA_SPI_FUNC gpio_set_function(_ILI9341_SDATA, GPIO_FUNC_SPI)
 
 //internal funcs
 static void ili9341_writeCommand(uint8_t commandByte);
 static void ili9341_writeData(uint8_t dataByte);
 static void ili9341_writeData(uint8_t* dataByte, size_t len);
+
+static void ili9341_hard_reset(); //bitbang reset
 
 // 1 powerup & initialization routine
 void ili9341_initialize(int8_t,int8_t,int8_t,int8_t,int8_t);
@@ -81,4 +91,5 @@ void ili9341_setAddrWindow(uint8_t,uint8_t,uint8_t,uint8_t);
 // utility functions
 void ili9341_drawFrame(uint32_t * buf, size_t len); //gp frame write; should be compatible with dma
 void ili9341_write_565(uint32_t * bmpData, size_t len); //draw a bitmap from ram; optimized and shii
+
 
--- lib/ili9341/src/ili9341.c
+++ lib/ili9341/src/ili9341.c
@@ -1,75 +1,63 @@
 #include "ili9341.h"
 
-static void st7735_writeCommand(uint8_t commandByte){
-    gpio_write(_ST7735_DC, false);
-    gpio_write(_ST7735_CS, false);
+static void ili9341_writeCommand(uint8_t commandByte){
+    gpio_write(_ILI9341_DC, false);
+    gpio_write(_ILI9341_CS, false);
     spi_write_blocking(spi0, &commandByte, 1);
-    gpio_write(_ST7735_CS, true);
+    gpio_write(_ILI9341_CS, true);
 }
 
-static void st7735_writeData(uint8_t dataByte){
-    gpio_write(_ST7735_DC, true);
-    gpio_write(_ST7735_CS, false);
+static void ili9341_writeData(uint8_t dataByte){
+    gpio_write(_ILI9341_DC, true);
+    gpio_write(_ILI9341_CS, false);
     spi_write_blocking(spi0, &dataByte, 1);
-    gpio_write(_ST7735_CS, true);
+    gpio_write(_ILI9341_CS, true);
 }
 
-static void st7735_writeData_Buffer(unit8_t* dataBuf, uint32_t len){
-    gpio_write(_ST7735_DC, true);
-    gpio_write(_ST7735_CS, false);
+static void ili9341_writeData_Buffer(unit8_t* dataBuf, uint32_t len){
+    gpio_write(_ILI9341_DC, true);
+    gpio_write(_ILI9341_CS, false);
     spi_write_blocking(spi0, dataBuf, len);
-    gpio_write(_ST7735_CS, true);
+    gpio_write(_ILI9341_CS, true);
 }
 
-static void st7735_reset(){
-    gpio_write(_ST7735_RST, true);
+static void ili9341_hard_reset(){
+    gpio_write(_ILI9341_RST, true);
     sleep_ms(10);
-    gpio_write(_ST7735_RST, false);
+    gpio_write(_ILI9341_RST, false);
     sleep_ms(10);
-    gpio_write(_ST7735_RST, true);
+    gpio_write(_ILI9341_RST, true);
     sleep_ms(10);
 }
 
-void st7735_initialize(int8_t rst, int8_t dc, int8_t cs, int8_t sclk, int8_t sdata){
-    _ST7735_RST = rst;
-    _ST7735_DC = dc;
-    _ST7735_CS = cs;
-    _ST7735_SCLK = sclk;
-    _ST7735_SDATA = sdata;
+void ili9341_initialize(int8_t rst, int8_t dc, int8_t cs, int8_t sclk, int8_t sdata){
+    _ILI9341_RST = rst;
+    _ILI9341_DC = dc;
+    _ILI9341_CS = cs;
+    _ILI9341_SCLK = sclk;
+    _ILI9341_SDATA = sdata;
  
-     // 1 perform gpio initialization
     _RST_INIT;
     _DC_INIT;
     _CS_INIT;
     _SCLK_INIT;
     _SDATA_INIT;   
    
+    gpio_write(_ILI9341_DC, 0);
+    gpio_write(_ILI9341_CS, 1);
+
     _RST_SetDigitalOut;
     _DC_SetDigitalOut;
     _CS_SetDigitalOut;
     _SCLK_SetDigitalOut;
     _SDATA_SetDigitalOut;
 
-    st7735_reset();
-
     //initialize hardware spi instance type and speed
     spi_init(spi0, 8000 * 1000); //spi freq @ 8Mhz
-    TFT_SCLK_SPI_FUNC; 
+    TFT_SCLK_SPI_FUNC;
     TFT_SDATA_SPI_FUNC;
 
-    /*
-    // 2 screen setup
-    //these adjustments to the lcd screen
-    _col_start = 2;
-    _row_start = 2; 
-    //width & height
-    _SCREEN_WIDTH = 128;
-    _SCREEN_HEIGHT = 160;
-    */
-    
-    //3 True hardware initialization Routine for 'ST7735R Black Tab'
+    ili9341_hard_reset(); //will reset all vram to random
 
-    //research the initialization routines and etc
-    gpio_write(_ST7735_DC, 0);
-    gpio_write(_ST7735_CS, 1);
+    
 }
