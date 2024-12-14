9a0cded15edb5bebf21304d7748985a51dbdedf9
plebish <jasoncast880@gmail.com>
2024-12-13 21:14:44 -0500

    asset files are now provided by ampalaya-tools

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#include "bgrDemo.h"
+//#include "bgrDemo.h"
+#include "my_image.h"
 #include "ili9341.h"
 
 //serves as a testbench for the things
@@ -15,7 +16,8 @@ int main() {
     ili9341_initialize(17,20,21,19,6,16);
 
     ili9341_writeCommand(RAM_WR);
-    ili9341_writeDataBuffer((uint8_t*)ili9341_Demo1, sizeof(ili9341_Demo1));
+    //ili9341_writeDataBuffer((uint8_t*)ili9341_Demo1, sizeof(ili9341_Demo1));
+    ili9341_writeDataBuffer((uint8_t*)my_image, sizeof(my_image));
     ili9341_writeCommand(NOOP);
 
 }
