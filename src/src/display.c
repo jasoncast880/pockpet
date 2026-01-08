03047ca559651641390b33182133d5d68730baed
plebish <jasoncast880@gmail.com>
2026-01-08 14:54:20 -0500

    TODO add to docs and finish the rough out of display

--- src/src/display.c
+++ src/src/display.c
@@ -2,6 +2,7 @@
 #include "ili9341.h"
 #include "portmacro.h"
 #include "projdefs.h"
+#include <hardware/dma.h>
 
 void display_setup() {
 
@@ -18,8 +19,7 @@ void control_block_handler() {
 
 }
 
-void lcd_render(void* pvParameters) {
-	//do tile engine initializations, protect with a semaphore.
+void lcd_render(void* pvParameters) { //do tile engine initializations, protect with a semaphore.
 	
 	for(;;) {
 		
@@ -27,9 +27,6 @@ void lcd_render(void* pvParameters) {
 }
 
 void lcd_write(void* pvParameters) { //queue reciever
-
-	int dma_chan = dma_claim_unused_channel(true);
-	dma_channel_set_irq0_enabled(dma_chan, true);
 
 	//ASSUMES SPI IS UNINITIALIZED 
 	spi_init(spi0, 8000 * 1000); //spi freq @ 8Mhz 
@@ -54,3 +51,26 @@ void lcd_write(void* pvParameters) { //queue reciever
 }
 
 void control_block_hanandl
+
+int write_block(uint16_t* buf, size_t size) {
+	int data_chan = dma_claim_unused_channel(true);
+
+	//channel configure
+	dma_channel_config c = dma_channel_get_default_config(data_chan);
+	channel_config_set_transfer_data_size(&c, DMA_SIZE_16); //for sending 565-pixel data 
+	channel_config_set_dreq(&c, uint dreq); //
+	//chain dependent on the display item; IS IT TILED OR IS IT PURE BUFFER?
+
+	channel_config_set_chain_to(dma_channel_config_t *c, uint chain_to); ///WRAP IN CONDITIONALS
+																																			 
+	channel_config_set_irq_quiet(&c, true);
+
+	dma_channel_configure(data_chan,
+		&c,
+		volatile void *write_addr,
+		const volatile void *read_addr,
+		0,
+		true //will start the channel imediately
+		);
+	//note this i within thread
+}
