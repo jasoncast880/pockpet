2790ef25d89240567337e5d121213eff2618489e
plebish <jasoncast880@gmail.com>
2026-03-30 20:33:39 -0400

    main blink test

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
 #include <cstddef>
-#include <hardware/gpio.h>
 #include <pico/stdio.h>
 #include <stdio.h>
-#include "class/cdc/cdc_device.h"
 #include "pico/stdlib.h"
-#include "pinout.h"
-
-#include "FreeRTOS.h"
-
-#include "projdefs.h"
-#include "task.h"
-#include "semphr.h"
-#include "queue.h"
 
 enum {
 	BLINK_NOT_MOUNTED = 20,
@@ -20,38 +10,24 @@ enum {
 };
 static uint32_t BLINK_INTERVAL_MS = BLINK_NOT_MOUNTED;
 
-//blink for status indicator
+#include "pico/cyw43_arch.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
 void main_task(void *pvParameters) {
-	for( ;; ){
-		vTaskDelay(pdMS_TO_TICKS(BLINK_INTERVAL_MS));
-		//blink();
-	}
-}
+    hard_assert(cyw43_arch_init() == PICO_OK);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    for( ;; ){
+        vTaskDelay(pdMS_TO_TICKS(BLINK_INTERVAL_MS));
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(BLINK_INTERVAL_MS));
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
 
-#include "buttons.h"
-#include "display.h"
-#include "usb.h"
-
-int main() {
-
-	sleep_ms(5000);
-	stdio_init_all();
-	printf("START");
-
-	xTaskCreate( main_task, "main_task", (configMINIMAL_STACK_SIZE * 4) , NULL, tskIDLE_PRIORITY+1, NULL );
-//	xTaskCreate( display_task, "display", 5000, NULL, tskIDLE_PRIORITY+1, NULL );
-	xTaskCreate( usb_task, "usb_task", (configMINIMAL_STACK_SIZE * 4) , NULL, tskIDLE_PRIORITY+1, NULL );
-//	xTaskCreate( button_task, "buttons_task", (configMINIMAL_STACK_SIZE * 4) , NULL, tskIDLE_PRIORITY+1, NULL );
-
-	vTaskStartScheduler();
-
-	while(1) {
-		tight_loop_contents();
-	}
+    }
 }
 
 extern "C" {
-
 	void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName ) {
 		printf("%s Task Stack Overflow failed\n", pcTaskName);
 		while(1);
@@ -64,3 +40,29 @@ extern "C" {
 	}
 
 }
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "buttons.h"
+#include "display.h"
+#include "usb.h"
+int main() {
+
+	sleep_ms(5000);
+	stdio_init_all();
+	printf("START");
+
+	xTaskCreate( main_task, "main_task", (configMINIMAL_STACK_SIZE * 6) , NULL, tskIDLE_PRIORITY+1, NULL );
+//  xTaskCreate( usb_task, "usb_task", (configMINIMAL_STACK_SIZE * 4) , NULL, tskIDLE_PRIORITY+1, NULL );
+//	xTaskCreate( button_task, "buttons_task", (configMINIMAL_STACK_SIZE * 4) , NULL, tskIDLE_PRIORITY+1, NULL );
+//	xTaskCreate( display_task, "display", 5000, NULL, tskIDLE_PRIORITY+1, NULL );
+
+	vTaskStartScheduler();
+
+	while(1) {
+		tight_loop_contents();
+	}
+}
+
+
