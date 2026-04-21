09335f3a3c3022fa81add38bea72bbe18ac9851a
plebish <jasoncast880@gmail.com>
2026-04-21 17:30:55 -0400

    sanity check pwm/pin for testing

--- src/dummy.c
+++ src/dummy.c
@@ -1,6 +1,9 @@
+#include <hardware/gpio.h>
+#include <pico/platform/common.h>
 #include <pico/stdio.h>
 #include <stddef.h>
 #include <hardware/spi.h>
+#include <hardware/pwm.h>
 #include <stdio.h>
 #include "pico/stdlib.h" 
 #include "pinout.h"
@@ -15,34 +18,15 @@ int main() {
 	sleep_ms(5000);
 	printf("GO");
 
-	display_setup();
+	gpio_set_function(2, GPIO_FUNC_PWM);
+	uint slice = pwm_gpio_to_slice_num(2);
 
-	uint8_t blue[2] = {0x00,0x1f};
-	uint8_t red[2]  = {0xf8,0x00};
+	pwm_set_wrap(slice, 1000);              // frequency control
+	pwm_set_chan_level(slice, PWM_CHAN_A, 500); // 50% duty cycle
 
-	for(int i = 0 ; i < 10 ; i++) {
-		ili9341_setCS_LO();
-
-		ili9341_setAddrWindow(0, i * DEFAULT_TILE_LEN, DEFAULT_TILE_LEN, DEFAULT_TILE_LEN);
-		for(int j = 0 ; j < DEFAULT_TILE_LEN * DEFAULT_TILE_LEN ; j++) {
-			ili9341_writeData(blue[0]);
-			ili9341_writeData(blue[1]);
-		}
-
-		ili9341_setCS_HI();
-
-		sleep_ms(100);
-
-		ili9341_setCS_LO();
-
-		ili9341_setAddrWindow(DEFAULT_TILE_LEN, i * DEFAULT_TILE_LEN, DEFAULT_TILE_LEN, DEFAULT_TILE_LEN);
-		for(int j = 0 ; j < DEFAULT_TILE_LEN * DEFAULT_TILE_LEN ; j++) {
-			ili9341_writeData(red[0]);
-			ili9341_writeData(red[1]);
-		}
-
-		ili9341_setCS_HI();
-
-	}
+	pwm_set_enabled(slice, true);
 	
+	while(1) {
+		tight_loop_contents();
+	}
 }
